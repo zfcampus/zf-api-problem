1ec4c879a2fd917f4c69e18cfffd36cd83bede95
Matthew Weier O'Phinney <matthew@zend.com>
2013-12-07 11:20:45 -0600

    Moved Module classfile to top-level directory
    - Use composer.json classmap to load Module classfile

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -4,7 +4,7 @@
  * @copyright Copyright (c) 2013 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
-namespace ZFTest;
+namespace ZFTest\ApiProblem;
 
 use Zend\Loader\AutoloaderFactory;
 use RuntimeException;
@@ -50,7 +50,7 @@ class Bootstrap
                     'autoregister_zf' => true,
                     'namespaces' => array(
                         'ZF\ApiProblem' => __DIR__ . '/../src/ZF/ApiProblem/',
-                        __NAMESPACE__ => __DIR__ . '/' . __NAMESPACE__,
+                        __NAMESPACE__ => __DIR__ . '/ZFTest/ApiProblem/',
                     ),
                 ),
             ));
