c502d158a58f5948e7950d746e1cd82ce60a2e19
Matthew Weier O'Phinney <matthew@zend.com>
2016-07-06 14:27:34 -0500

    Ensure new `Module` tests pass against all versions
    - Required adapting the `ModuleTest` to vary the `EventManager` construction
      based on features detected.
    - Also recognized that with the `Module` class in a subdirectory, the path to
      the config file needed to change.

--- test/ModuleTest.php
+++ test/ModuleTest.php
@@ -9,6 +9,7 @@
 namespace ZF\ApiProblem;
 
 use PHPUnit_Framework_TestCase as TestCase;
+use ReflectionClass;
 use Zend\EventManager\EventManager;
 use Zend\EventManager\SharedEventManager;
 use Zend\Mvc\Application;
@@ -20,6 +21,17 @@ use ZF\ApiProblem\Listener\SendApiProblemResponseListener;
 
 class ModuleTest extends TestCase
 {
+    public function marshalEventManager()
+    {
+        $r = new ReflectionClass(EventManager::class);
+        if ($r->hasMethod('setSharedManager')) {
+            $eventManager = new EventManager();
+            $eventManager->setSharedManager(new SharedEventManager());
+            return $eventManager;
+        }
+        return new EventManager(new SharedEventManager());
+    }
+
     public function testOnBootstrap()
     {
         $module = new Module();
@@ -28,7 +40,7 @@ class ModuleTest extends TestCase
         $serviceLocator = $this->getMockForAbstractClass(ServiceLocatorInterface::class);
         $serviceLocator->method('get')->will($this->returnCallback([$this, 'serviceLocator']));
 
-        $eventManager = new EventManager(new SharedEventManager());
+        $eventManager = $this->marshalEventManager();
         $event = $this->getMock(MvcEvent::class);
 
         $application->method('getServiceManager')->willReturn($serviceLocator);
